916e74a4edbdb56aa1b70cc6e9e1de262d959a72
aixinjian <229380496@qq.com>
2018-10-08 17:47:54 +0800

    first commit

---
+++ src/main/java/com/learning/base/AtLearningBaseApplication.java
@@ -0,0 +1,12 @@
+package com.learning.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtLearningBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtLearningBaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learning/base/AtLearningBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.base;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtLearningBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
